1d29f50df67fc20c7176db172f7f2ef086029a06
Seppe Gadeyne <seppe@fushia.be>
2020-02-20 12:54:03 +0100

    Update

--- groeiproject/02_node_basics/sub.js
+++ groeiproject/02_node_basics/sub.js
@@ -4,10 +4,11 @@ const json = require('../persons');
 const fs = require('fs');
 
 function writeSub(personId, cars) {
-    if (!personId || !cars) return console.error("Error: no arguments for writeSub();");
+    if (!personId || !cars) return console.error('Error: no arguments for writeSub();');
 
     const person = json.persons.filter(person => person.id === personId);
     let filename = null;
+
     if (person[0].first_name) filename = `${person[0].first_name}_`;
     if (person[0].last_name) filename += `${person[0].last_name}_`;
     filename += `${person[0].id}.json`;
